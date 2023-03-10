cbd78a1bcff0a2e1c2f53d3855d479be5815c850
Vanesa Hernandez <vanesahca@gmail.com>
2023-03-10 13:24:14 -0500

    Adding the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <header className="ng-scope ng-isolate-scope">
+        <nav className="navbar navbar-light">
+          <div className="container">
+            <a href="???" className="navbar-brand ng-binding">
+              conduit
+            </a>
+            <ul className="nav navbar-nav pull-xs-right">
+              <li className="nav-item">
+                <a href="???" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="???" className="nav-link">
+                  Sign in
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="???" className="nav-link">
+                  Sign up
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="home-page ng-scope">
+        <div className="banner">
+          <div className="container">
+            <h1 className="logo-font ng-binding">conduit</h1>
+            <p>A place to share your knowledge</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
